0dd62045ff9fbebd7e49c83e9b3725aa5ed5f0e0
ljy2855 <ljy2855@naver.com>
2022-05-09 16:32:06 +0900

    feat: Redesign Layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { ClubResType, LoginReqType, WishResType } from "../types";
 import useAccessToken from "../hooks/useAccessToken";
-import styles from "./Main.module.scss";
-import { Container, Navbar } from "react-bootstrap";
+import { Col, Container, Navbar, Row } from "react-bootstrap";
 
 import LoginButton from "./LoginButton";
-import MainSideBar from "./MainSideBar";
+import PageSideBar from "./PageSideBar";
 import FilterClub from "./FilterClub";
 import { Route, Switch } from "react-router-dom";
 import UserAsk from "./UserAsk";
 import NotFound from "../pages/NotFound";
 import ClubList from "./ClubList";
 import JubroLab from "./JubroLab";
+import RightSideBar from "./RightSideBar";
+
+import styles from "./Main.module.scss";
 
 interface MainProps {
   wishs: WishResType[] | null;
@@ -54,97 +56,42 @@ const Main: React.FC<MainProps> = ({
 
   const [clubs, setClubs] = useState<ClubResType[]>([]);
 
-  const [isSideBarOpen, setSideBarOpen] = useState(true);
-
-  function Home() {
-    return (
-      <>
-        <div className="flex-column  ">
-          <p className="text-center">서강 아리</p>
-          <div className="container">
-            <div className={styles.main_subtitle}>
-              환영합니다, <strong>서강</strong>대학교 동<strong>아리</strong>
-              플랫폼입니다
-              {" | "}
-              <a href="https://www.instagram.com/jooeon.kang/?hl=ko">
-                @Team_Luwak
-              </a>
-              <br />
-              <br />
-              동아리 정보를 손쉽게 받아가세요
-              <br />
-            </div>
-          </div>
-          <FilterClub setClubs={setClubs} />
-          {/* <div className="text-center">
-            <img
-              src="/images/sogang_bg_remove_small.png"
-              className={styles.logoImage}
-            ></img>
-          </div> */}
-          <ClubList clubs={clubs} />
-        </div>
-      </>
-    );
-  }
-  // TODO #1 sidebar collapse
-  // TODO #2 resize main_wrapper when hide sidebar
   return (
     <>
-      {
-        <Container>
-          <MainSideBar isOpen={isSideBarOpen} />
-          <div
-            className={
-              isSideBarOpen ? styles.main_wrapper : styles.main_wrapper_expend
-            }
-          >
-            <nav className="navbar navbar-inverse navbar-fixed-top">
-              {/* <Button
-                variant="outline-secondary"
-                onClick={() => setSideBarOpen(!isSideBarOpen)}
-                aria-controls="sidebar-collapse"
-                aria-expanded={isSideBarOpen}
-              >
-                <i className="bi bi-list"></i>
-              </Button> */}
-              <Navbar.Toggle
-                onClick={() => setSideBarOpen(!isSideBarOpen)}
-                aria-controls="main_sidebar"
-                aria-expanded={isSideBarOpen}
-              >
-                <i className="bi bi-list"></i>
-              </Navbar.Toggle>
-              <LoginButton
-                logout={logout}
-                login={login}
-                error={auth_error}
-                loading={auth_loading}
-              />
-            </nav>
-            <div className={styles.Main}>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/ask" component={UserAsk} />
-                <Route
-                  exact
-                  path="/jubroLab"
-                  render={() => (
-                    <JubroLab
-                      wishs={wishs}
-                      wish_error={wish_error}
-                      wish_loading={wish_loading}
-                      deleteWish={deleteWish}
-                      addWish={addWish}
-                    />
-                  )}
-                />
-                <Route component={NotFound} />
-              </Switch>
-            </div>
-          </div>
-        </Container>
-      }
+      <Container fluid className="mt-5 mx-1">
+        <Row>
+          <Col className="col-3">
+            <PageSideBar></PageSideBar>
+          </Col>
+          <Col>
+            <Container className={styles.logo_wrap}>
+              <div>
+                <div className={styles.logo}>SogangAri</div>
+                <img
+                  className={styles.logo_img}
+                  src="images/sogang_ari_logo.png"
+                ></img>
+              </div>
+              <div className={styles.description}>
+                Platform for club activities at
+                <br />
+                Sogang University
+              </div>
+            </Container>
+            <Container>
+              <FilterClub setClubs={setClubs} />
+            </Container>
+          </Col>
+          <Col className="col-3">
+            <RightSideBar
+              logout={logout}
+              login={login}
+              error={auth_error}
+              loading={auth_loading}
+            ></RightSideBar>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import MainTest from "../components/MainTest";
-import MainContainer from "../containers/MainContainer";
 
+import MainContainer from "../containers/MainContainer";
 export default function Home() {
   return <MainContainer />;
 }
